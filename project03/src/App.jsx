77e9ebda246c635c385a5d97c1c233ed6d9c76f3
Akash Karan <akashkaran11@gmail.com>
2026-01-14 09:04:40 +0530

    Added project03 as normal folder

---
+++ project03/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import BgChanger from "./component/BgChanger";
+import PasswordGenerator from "./component/PasswordGenerator";
+
+function App() {
+  return (
+    <>
+      {/* <BgChanger /> */}
+      <PasswordGenerator />
+    </>
+  );
+}
+
+export default App;
